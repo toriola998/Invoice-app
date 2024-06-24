2015df9a62745b718906d0b7378b146408ae74ad
Toriola <faaedah@gmail.com>
2024-06-24 12:14:47 +0100

    SETUP: scafolded project

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './styles/index.scss'
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/routes/Home.jsx
@@ -0,0 +1,5 @@
+export default function Home () {
+    return (
+        <h1 className="text-red-500 underline">Hello World</h1>
+    )
+}
