9a58696fb8474b8a2bccd9bd60c868b49d8f6cf8
Toriola <faaedah@gmail.com>
2024-06-25 13:18:22 +0100

    ADD: added buttons

--- src/components/CreateInvoice.jsx
+++ src/components/CreateInvoice.jsx
@@ -7,6 +7,21 @@ export default function CreateInvoice() {
          <div className="mb-9">
             <p className="bold-15 text-blue my-6 md:mt-12">Bill From</p>
          </div>
+
+         <div className="bottom-btns">
+            <div className="flex justify-between control-btn">
+               <button className="bg-light-2 text-grey-2">Discard</button>
+               <div className="flex gap-2 ">
+                  <button className="text-grey bg-black-6">
+                     Save as Draft
+                  </button>
+
+                  <button className="bg-blue">
+                     <span>Save & Send</span>
+                  </button>
+               </div>
+            </div>
+         </div>
       </InvoiceLayout>
    );
 }
