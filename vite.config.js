07f0311eec2d22dd7384f977f2d82d94d4e039f4
Toriola <faaedah@gmail.com>
2024-07-16 23:37:32 +0100

    ADD: added redux toolkit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-   build: {
-      rollupOptions: {
-         external: ['react-redux'],
-      },
-   },
+   // build: {
+   //    rollupOptions: {
+   //       external: ['react-redux'],
+   //    },
+   // },
    // resolve: {
    //    alias: {
    //      'react-redux': require.resolve('react-redux')
