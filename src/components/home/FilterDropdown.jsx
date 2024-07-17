997c10fafd17425076667d800a9451ec2d880f63
Toriola <faaedah@gmail.com>
2024-07-17 14:48:44 +0100

    REFACTOR: replaced dropdown to radix

--- src/components/home/FilterDropdown.jsx
+++ src/components/home/FilterDropdown.jsx
@@ -1,3 +1,4 @@
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addFilter, removeFilter } from '../../store/invoiceSlice';
@@ -5,14 +6,6 @@ import { addFilter, removeFilter } from '../../store/invoiceSlice';
 function FilterDropdown() {
    const [dropdown, setDropdown] = useState(false);
    const dispatch = useDispatch();
-
-   function toggleDropdown() {
-      setDropdown(true);
-
-      if (dropdown === true) {
-         setDropdown(false);
-      }
-   }
 
    function setDraftAsFilter(event) {
       const filterValue = event.target.checked;
@@ -42,20 +35,26 @@ function FilterDropdown() {
    }
 
    return (
-      <div className="relative">
-         <button className="font-medium flex-center" onClick={toggleDropdown}>
-            <span className="mr-3 flex-center">
-               <span className="mr-1">Filter </span>
-               <span className="hidden md:flex">by status</span>
-            </span>
-            <img
-               src="../../assets/icons/icon-dropdown.svg"
-               alt=""
-               className={dropdown ? 'rotate-180 ease-in duration-100' : ''}
-            />
-         </button>
-         {dropdown && (
-            <div className="absolute bg-black-2 dropdown p-6 rounded-lg w-[190px] -left-20 md:-left-8 top-12 z-20">
+      <DropdownMenu.Root>
+         <DropdownMenu.Trigger asChild>
+            <button className="font-medium flex-center">
+               <span className="mr-3 flex-center">
+                  <span className="mr-1">Filter </span>
+                  <span className="hidden md:flex">by status</span>
+               </span>
+               <img
+                  src="../../assets/icons/icon-dropdown.svg"
+                  alt=""
+                  className={dropdown ? 'rotate-180 ease-in duration-100' : ''}
+               />
+            </button>
+         </DropdownMenu.Trigger>
+
+         <DropdownMenu.Portal>
+            <DropdownMenu.Content
+               sideOffset={5}
+               className="dropdown bg-black-2 p-6 rounded-lg w-[190px]"
+            >
                <div>
                   <input
                      type="checkbox"
@@ -78,9 +77,9 @@ function FilterDropdown() {
                   <input type="checkbox" id="paid" onChange={setPaidAsFilter} />
                   <label htmlFor="paid">Paid</label>
                </div>
-            </div>
-         )}
-      </div>
+            </DropdownMenu.Content>
+         </DropdownMenu.Portal>
+      </DropdownMenu.Root>
    );
 }
 
