10c21070178d17a1773be781b2d3f73bcb46aac3
Toriola <faaedah@gmail.com>
2024-07-17 12:09:23 +0100

    FIX: fixed date returning null

--- src/utils/date.js
+++ src/utils/date.js
@@ -26,7 +26,9 @@ const DATE = {
 
       // Format the date in the desired format "8 Sep 2023"
       const formattedDate = `${day} ${monthNames[monthIndex]} ${year}`;
-      return formattedDate;
+      if(inputDate) {
+         return formattedDate
+      }else return
    },
 
    getPaymentDueDate: (dateString, n) => {
@@ -39,7 +41,9 @@ const DATE = {
       // Format the date as YYYY-MM-DD
       const formattedDate = `${paymentNetDay.getFullYear()}-${String(paymentNetDay.getMonth() + 1).padStart(2, '0')}-${String(paymentNetDay.getDate()).padStart(2, '0')}`;
 
-      return DATE.formatDate(formattedDate);
+      if(dateString) {
+         return DATE.formatDate(formattedDate);
+      }else return
    },
 };
 
